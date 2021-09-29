688d17ba85bd211d6e062ee6e41564b09c1ab52f
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-09-28 19:13:24 -0500

    Add project files.

---
+++ Palindromes.test/UnitTest1.cs
@@ -0,0 +1,60 @@
+using System;
+using Xunit;
+
+namespace Palindromes.test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void single_words_read_the_same_forwards_and_backwards()
+        {
+            //arrange
+            var word = "radar";
+            var expectedResult = "radar";
+            var palindromeCheck = new PalindromeTester();
+            //act
+            var actualResult = palindromeCheck.Test(word);
+            //assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void multiple_words_read_the_same_forwards_and_backwards_but_with_no_spaces()
+        {
+            //arrange
+            var word = "race car";
+            var expectedResult = "racecar";
+            var palindromeCheck = new PalindromeTester();
+            //act
+            var actualResult = palindromeCheck.Test(word);
+            //assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void capital_letters_are_changed_to_lower_case()
+        {
+            //arrange
+            var word = "rAce Car";
+            var expectedResult = "racecar";
+            var palindromeCheck = new PalindromeTester();
+            //act
+            var actualResult = palindromeCheck.Test(word);
+            //assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Theory]
+        [InlineData("race car", "racecar")]
+        [InlineData("No X IN NIXON", "noxinnixon")]
+        public void test_if_a_given_string_is_a_palindrome(string input, string expectedResult)
+        {
+            //arrange
+            var palinderomTester = new PalindromeTester();
+            //act
+            var actualResult = palinderomTester.Test(input);
+            //assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+    }
+}
---
+++ Palindromes/PalindromeTester.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Palindromes
+{
+    public class PalindromeTester
+    {
+        public string Test(string stringToTest)
+        {
+            string str = Regex.Replace(stringToTest.ToLower(), @"\s", "");
+            char[] charArray = str.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
